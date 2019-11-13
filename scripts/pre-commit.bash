a9f39762ae153c76f8895eb4af621bcf4d66e674
Guilherme <guilhermehas@hotmail.com>
2019-11-13 00:55:13 -0300

    add new hook

--- scripts/pre-commit.bash
+++ scripts/pre-commit.bash
@@ -1,7 +1,6 @@
 #!/usr/bin/env bash
 
-# nix-build
-echo "Commiting"
+nix-build -I nixpkgs=https://github.com/NixOS/nixpkgs-channels/archive/nixos-18.09.tar.gz
 
 # $? stores exit value of the last command
 if [ $? -ne 0 ]; then
