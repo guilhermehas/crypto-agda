a76d487f93a2646932fd5536b498cfae088dc943
Guilherme <guilhermehas@hotmail.com>
2019-11-19 03:29:53 -0300

    copy files instead of links

--- copylatexout.bash
+++ copylatexout.bash
@@ -1,1 +1,1 @@
-cd $(dirname $(dirname $(which latexmk)))/share/texmf/tex/latex && cp -r *.tex $1 && cp agda.sty $2
+cd $(dirname $(dirname $(which latexmk)))/share/texmf/tex/latex && cp -Lr *.tex $1 && cp agda.sty $2
