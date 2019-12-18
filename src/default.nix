d6bf7d83f210e66f26099a9200bb5440c2eefd4f
Guilherme <guilhermehas@hotmail.com>
2019-12-18 19:40:34 -0300

    modified build library

--- src/default.nix
+++ src/default.nix
@@ -1,11 +1,27 @@
 { pkgs ? import <nixpkgs> {} }: with pkgs;
 
 let
-  prelude = builtins.fetchGit rec {
-    name = "agda-prelude";
-    url = "https://github.com/NixOS/nixpkgs.git";
-    rev = "d193a94bfac9505148cb5cd1b68d08a08260b59c";
-  };
+  prelude =
+    agda.mkDerivation (self: rec {
+      version = "d193a94bfac9505148cb5cd1b68d08a08260b59c";
+      name = "agda-prelude-${version}";
+
+      src = fetchGit {
+        url = "https://github.com/UlfNorell/agda-prelude.git";
+        rev = version;
+      };
+
+      topSourceDirectories = [ "src" ];
+      everythingFile = "src/Prelude.agda";
+
+      meta = with stdenv.lib; {
+        homepage = https://github.com/UlfNorell/agda-prelude;
+        description = "Programming library for Agda";
+        license = stdenv.lib.licenses.mit;
+        platforms = stdenv.lib.platforms.unix;
+        maintainers = with maintainers; [ fuuzetsu mudri ];
+      };
+    });
 in
 stdenv.mkDerivation rec {
   name = "agdapkg";
