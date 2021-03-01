d1242c5e5d4d9ccbb96f04f39930ad640d6663bd
Guilherme <guilhermehas@hotmail.com>
2021-03-01 18:34:34 -0300

    changed default.nix to use nixFlakes

--- default.nix
+++ default.nix
@@ -1,22 +1,3 @@
-{ pkgs ? import <nixpkgs> {}
-, nixos-18 ? import <nixos-18> {}
-}: with pkgs;
-
-let
-  agdapkg = nixos-18.callPackage src/default.nix {};
-in
-stdenv.mkDerivation {
-  name = "master-thesis";
-  src = ./.;
-  buildInputs = [
-    which
-    (texlive.combine {
-        inherit (texlive)
-          scheme-full
-        ;
-        simple-package = {
-          pkgs = [ agdapkg ];
-        };
-      })
-  ];
-}
+(import (fetchTarball https://github.com/edolstra/flake-compat/archive/master.tar.gz) {
+  src = builtins.fetchGit ./.;
+}).defaultNix
--- flake.nix
+++ flake.nix
@@ -1,15 +1,34 @@
 {
-  description = "A flake for building Hello World";
+  description = "Master thesis of cryptocurrency in Agda";
 
+  inputs.flake-utils.url = github:numtide/flake-utils;
   inputs.nixos-18 = {
     url = github:NixOS/nixpkgs/nixos-18.09;
     flake = false;
   };
 
-  outputs = { self, nixpkgs, nixos-18 }: {
-    defaultPackage.x86_64-linux = import ./default.nix {
-      pkgs = import nixpkgs { system = "x86_64-linux"; };
-      nixos-18 = import nixos-18 { system = "x86_64-linux"; };
-    };
-  };
+  outputs = { self, flake-utils, nixpkgs, nixos-18 }:
+    flake-utils.lib.eachDefaultSystem (system:
+    let pkgs = import nixpkgs { inherit system; };
+        nixos18 = import nixos-18 { inherit system; };
+    in rec {
+      packages.thesis = with pkgs;
+        let agdapkg = nixos18.callPackage src/default.nix {};
+        in stdenv.mkDerivation {
+            name = "master-thesis";
+            src = ./.;
+            buildInputs = [
+              which
+              (texlive.combine {
+                  inherit (texlive)
+                    scheme-full
+                  ;
+                  simple-package = {
+                    pkgs = [ agdapkg ];
+                  };
+                })
+            ];
+          };
+      defaultPackage = packages.thesis;
+    });
 }
